1e42ace8415bbbac86baed13b1fb98ca819990a3
rosakim1101 <rosakim1101@naver.com>
2020-11-06 10:15:43 +0900

    mang ham

--- app/src/main/java/com/example/helpick/ItemAdapter.java
+++ app/src/main/java/com/example/helpick/ItemAdapter.java
@@ -3,6 +3,7 @@ package com.example.helpick;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,23 +12,45 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>implements OnPickItemClickListener {
 
     ArrayList<Item> items = new ArrayList<>();
+    OnPickItemClickListener listener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
     static class ViewHolder extends RecyclerView.ViewHolder{
         TextView tv1;
         Button btn1, btn2;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, final OnPickItemClickListener listener) {
+
             super(itemView);
 
             tv1 = itemView.findViewById(R.id.item_name);
             btn1 = itemView.findViewById(R.id.item_btn1);
             btn2 = itemView.findViewById(R.id.item_btn2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    if (listener != null){
+                        listener.onItemClick(ViewHolder.this, v,position);
+                    }
+                }
+            });
+
+        }
+
+        public void setItem(Item item){
+            tv1.setText(item.getName());
         }
 
     }
-
 
     @NonNull
     @Override
@@ -40,10 +63,22 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Item item = items.get(position);
+        holder.setItem(item);
     }
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
+
+    public void addItem(Item item){items.add(item);}
+    public void setItems(ArrayList<Item> items){this.items=items;}
+    public Item getItem(int position){return items.get(position);}
+    public void setItem(int position, Item item){items.set(position, item);}
+
+    @Override
+    public void onItemClick(ViewHolder holder, View view, int position) {
+
+    }
+
 
 }
--- app/src/main/java/com/example/helpick/MainActivity.java
+++ app/src/main/java/com/example/helpick/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.helpick;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,7 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ItemAdapter adapter = new ItemAdapter();
+
+        adapter.addItem(new Item("제비뽑기"));
+        adapter.addItem(new Item("가위바위보"));
+        adapter.addItem(new Item("돌림판"));
+        adapter.addItem(new Item("숫자뽑기"));
+
+        recyclerView.setAdapter(adapter);
 
     }
+
 }
